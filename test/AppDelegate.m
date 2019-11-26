01c9bc4db5e9f55c7d97b1f63bc7f85320383a61
jiashiqi <jiashiqi@zealfi.com>
2019-11-26 15:58:44 +0800

    format-qi
    fix

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -9,7 +9,6 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
@@ -28,8 +27,8 @@
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+     
 }
 
 
--- test/ViewController.m
+++ test/ViewController.m
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.m
 //  test
@@ -8,8 +9,9 @@
 
 #import "ViewController.h"
 #import "LocationController.h"
-@interface ViewController ()
-
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (nonatomic, strong)CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
@@ -27,9 +29,64 @@
     //第三步在.gpx中添加地图转换后的坐标
     
     //运行
-
+    
+    _locationManager =[[CLLocationManager alloc]init];
+    _locationManager.delegate =self;
+    //设置定位经准
+    _locationManager.desiredAccuracy =kCLLocationAccuracyNearestTenMeters;
+    [_locationManager requestWhenInUseAuthorization];//否则，ios10不弹定位框
+    _locationManager.distanceFilter =10.0f;
+    //开始定位
+    [_locationManager startUpdatingLocation];
+    
+}
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    
+    CLLocation *newLocation =locations[0];
+    
+    CLLocationCoordinate2D oCoordinate =newLocation.coordinate;
+    
+    [_locationManager stopUpdatingLocation];
+    
+    
+    
+    NSLog(@"经度------%f\n------纬度%f",oCoordinate.longitude,oCoordinate.latitude);
+    
+    //创建地理位置解码编码器对象
+    
+    CLGeocoder *geoCoder =[[CLGeocoder alloc]init];
+    
+    [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        for (CLPlacemark *place in placemarks) {
+            
+            NSLog(@"城市-----%@",place.locality);
+            
+        }
+        
+    }];
+    
 }
 
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    
+    if ([error code] == kCLErrorDenied){
+        
+        //访问被拒绝
+        
+        NSLog(@"访问被拒绝");
+        
+    }
+    
+    if ([error code] == kCLErrorLocationUnknown) {
+        
+        //无法获取位置信息
+        
+        NSLog(@"无法获取位置信息");
+        
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
