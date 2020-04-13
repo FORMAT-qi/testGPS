6e1ff256455c3d1b11c1310d22e2afe2d2c101e9
jiashiqi <jiashiqi@zealfi.com>
2020-04-13 11:24:05 +0800

    Update ViewController.m

--- test/ViewController.m
+++ test/ViewController.m
@@ -30,68 +30,68 @@
     
     //运行
     
-    _locationManager =[[CLLocationManager alloc]init];
-    _locationManager.delegate =self;
-    //设置定位经准
-    _locationManager.desiredAccuracy =kCLLocationAccuracyNearestTenMeters;
-    [_locationManager requestWhenInUseAuthorization];//否则，ios10不弹定位框
-    _locationManager.distanceFilter =10.0f;
-    //开始定位
-    [_locationManager startUpdatingLocation];
+//    _locationManager =[[CLLocationManager alloc]init];
+//    _locationManager.delegate =self;
+//    //设置定位经准
+//    _locationManager.desiredAccuracy =kCLLocationAccuracyNearestTenMeters;
+//    [_locationManager requestWhenInUseAuthorization];//否则，ios10不弹定位框
+//    _locationManager.distanceFilter =10.0f;
+//    //开始定位
+//    [_locationManager startUpdatingLocation];
     
 }
--(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
-    
-    CLLocation *newLocation =locations[0];
-    
-    CLLocationCoordinate2D oCoordinate =newLocation.coordinate;
-    
-    [_locationManager stopUpdatingLocation];
-    
-    
-    
-    NSLog(@"经度------%f\n------纬度%f",oCoordinate.longitude,oCoordinate.latitude);
-    
-    //创建地理位置解码编码器对象
-    
-    CLGeocoder *geoCoder =[[CLGeocoder alloc]init];
-    
-    [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-        
-        for (CLPlacemark *place in placemarks) {
-            
-            NSLog(@"城市-----%@",place.locality);
-            
-        }
-        
-    }];
-    
-}
-
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
-    
-    if ([error code] == kCLErrorDenied){
-        
-        //访问被拒绝
-        
-        NSLog(@"访问被拒绝");
-        
-    }
-    
-    if ([error code] == kCLErrorLocationUnknown) {
-        
-        //无法获取位置信息
-        
-        NSLog(@"无法获取位置信息");
-        
-    }
-    
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+//
+//    CLLocation *newLocation =locations[0];
+//
+//    CLLocationCoordinate2D oCoordinate =newLocation.coordinate;
+//
+//    [_locationManager stopUpdatingLocation];
+//
+//
+//
+//    NSLog(@"经度------%f\n------纬度%f",oCoordinate.longitude,oCoordinate.latitude);
+//
+//    //创建地理位置解码编码器对象
+//
+//    CLGeocoder *geoCoder =[[CLGeocoder alloc]init];
+//
+//    [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+//
+//        for (CLPlacemark *place in placemarks) {
+//
+//            NSLog(@"城市-----%@",place.locality);
+//
+//        }
+//
+//    }];
+//
+//}
+//
+//- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+//
+//    if ([error code] == kCLErrorDenied){
+//
+//        //访问被拒绝
+//
+//        NSLog(@"访问被拒绝");
+//
+//    }
+//
+//    if ([error code] == kCLErrorLocationUnknown) {
+//
+//        //无法获取位置信息
+//
+//        NSLog(@"无法获取位置信息");
+//
+//    }
+//
+//}
+//
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 
 @end
